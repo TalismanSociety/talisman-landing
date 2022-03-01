1ecfa8c0e7c087116dc63ca49a5bfcb0fa6babb3
juliuscarvajal <julius@talisman.xyz>
2022-03-02 06:49:28 +0800

    black nav back

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,44 +5,44 @@ window.onload = function () {
   menuIcon.checked = false;
 };
 
-// let lastScrollTop = 0;
-// window.onscroll = function () {
-//   const scrollY = window.pageYOffset || document.documentElement.scrollTop;
-//   if (
-//     document.body.scrollTop >= 50 ||
-//     document.documentElement.scrollTop >= 50
-//   ) {
-//     if (scrollY > lastScrollTop) {
-//       // downscroll
-//       stickyNav.classList.add("closed");
-//       menuIcon.checked = false;
-//     } else {
-//       // upscroll
-//       stickyNav.classList.remove("closed");
-//     }
-//   }
+let lastScrollTop = 0;
+window.onscroll = function () {
+  const scrollY = window.pageYOffset || document.documentElement.scrollTop;
+  if (
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
+  ) {
+    if (scrollY > lastScrollTop) {
+      // downscroll
+      stickyNav.classList.add("closed");
+      menuIcon.checked = false;
+    } else {
+      // upscroll
+      stickyNav.classList.remove("closed");
+    }
+  }
 
-//   lastScrollTop = scrollY <= 0 ? 0 : scrollY;
+  lastScrollTop = scrollY <= 0 ? 0 : scrollY;
 
-//   if (
-//     document.body.scrollTop >= 100 ||
-//     document.documentElement.scrollTop >= 100 ||
-//     menuIcon.checked
-//   ) {
-//     stickyNav.classList.add("nav-colored");
-//     stickyNav.classList.remove("nav-transparent");
-//   } else {
-//     stickyNav.classList.add("nav-transparent");
-//     stickyNav.classList.remove("nav-colored");
-//   }
-// };
+  if (
+    document.body.scrollTop >= 300 ||
+    document.documentElement.scrollTop >= 300 ||
+    menuIcon.checked
+  ) {
+    stickyNav.classList.add("nav-colored");
+    stickyNav.classList.remove("nav-transparent");
+  } else {
+    stickyNav.classList.add("nav-transparent");
+    stickyNav.classList.remove("nav-colored");
+  }
+};
 
-// menuIcon.addEventListener("change", function (e) {
-//   if (e.target.checked) {
-//     stickyNav.classList.add("nav-colored");
-//     stickyNav.classList.remove("nav-transparent");
-//   } else if (window.scrollY < 100) {
-//     stickyNav.classList.add("nav-transparent");
-//     stickyNav.classList.remove("nav-colored");
-//   }
-// });
+menuIcon.addEventListener("change", function (e) {
+  if (e.target.checked) {
+    stickyNav.classList.add("nav-colored");
+    stickyNav.classList.remove("nav-transparent");
+  } else if (window.scrollY < 100) {
+    stickyNav.classList.add("nav-transparent");
+    stickyNav.classList.remove("nav-colored");
+  }
+});
