6931223a6e5332ea11eacf4735cd2d2083406cbd
juliuscarvajal <julius@talisman.xyz>
2022-03-03 12:35:17 +0800

    fix

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,10 +1,36 @@
-var glide = new Glide(".glide");
+function DisableControls(Glide, Components, Events) {
+  const PREV_CONTROL_SELECTOR = "[data-glide-dir='<']";
+  const NEXT_CONTROL_SELECTOR = "[data-glide-dir='>']";
+  const component = {
+    buildAfter() {
+      this.prevBtn = Components.Html.root.querySelector(PREV_CONTROL_SELECTOR);
+      this.nextBtn = Components.Html.root.querySelector(NEXT_CONTROL_SELECTOR);
+    },
+    handleDisable() {
+      const perView = Glide.settings.perView;
+      const slidesCount = Components.Html.slides.length;
 
-// TODO: Hide left/right accordingly
-glide.on("mount.after", function (e) {
-  const leftArrow = document.getElementById("glide__arrow--left");
-  leftArrow.setAttribute("visibility", "hidden");
-  console.log(`???mount.after`, leftArrow);
+      this.prevBtn.disabled = Glide.index <= 0;
+      this.nextBtn.disabled = Glide.index >= slidesCount - perView;
+
+      this.prevBtn.style.opacity = this.prevBtn.disabled ? "0" : "0.8";
+      this.nextBtn.style.opacity = this.nextBtn.disabled ? "0" : "0.8";
+    },
+  };
+
+  Events.on("build.after", function () {
+    component.buildAfter();
+    component.handleDisable();
+  });
+
+  Events.on("run.after", function () {
+    component.handleDisable();
+  });
+  return component;
+}
+
+const glide = new Glide(".glide");
+
+glide.mount({
+  DisableControls,
 });
-
-glide.mount();
